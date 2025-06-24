31f20dc7a73dd2db6fd54cd50f7ba5553c28f9dd
lucas <lucaston2323@gmail.com>
2025-06-23 23:47:37 -0300

    fix: alterar rota padrão

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
         path: 'home',
         component: Home
     }
