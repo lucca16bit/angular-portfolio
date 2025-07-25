2f8a386f278163dc972e697e15b3a45daf0e4b7b
lucas <lucaston2323@gmail.com>
2025-07-24 23:33:07 -0300

    refactor: orgazinar código do componente das skills

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -1,13 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { Skill } from '../../interfaces/skill.interface';
 import { Svg } from '../svg/svg';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'skills',
-    imports: [Svg, TranslateModule],
+    imports: [Svg, TranslateModule, CommonModule],
     templateUrl: './skills.html'
 })
-export class Skills {
 
+export class Skills {
+    svgSize: number = 50;
+
+    skills: Skill[] = [
+        { icon: 'java', title: 'Java' },
+        { icon: 'spring', title: 'Spring Boot' },
+        { icon: 'nodejs', title: 'Node.js' },
+        { icon: 'express', title: 'Express' },
+        { icon: 'angular', title: 'Angular' },
+        { icon: 'react', title: 'React' },
+        { icon: 'html5', title: 'HTML5' },
+        { icon: 'css3', title: 'CSS3' },
+        { icon: 'typescript', title: 'Typescript' },
+        { icon: 'javascript', title: 'Javascript' },
+        { icon: 'sass', title: 'Sass/Scss' },
+        { icon: 'tailwind', title: 'TailwindCSS' },
+        { icon: 'mysql', title: 'MySQL' },
+        { icon: 'postgresql', title: 'PostgreSQL' },
+        { icon: 'docker', title: 'Docker' },
+        { icon: 'aws', title: 'AWS' },
+        { icon: 'oci', title: 'OCI' },
+        { icon: 'git', title: 'git' },
+        { icon: 'powershell', title: 'Powershell' },
+        { icon: 'bash', title: 'Bash' },
+        { icon: 'postman', title: 'Postman' },
+        { icon: 'swagger', title: 'Swagger' },
+        { icon: 'maven', title: 'Maven' },
+        { icon: 'gradle', title: 'Gradle' },
+        { icon: 'npm', title: 'npm' }
+    ];
+
+    @HostListener('window:resize')
+    onResize() {
+        this.updateSvgSize();
+    }
+
+    ngOnInit() {
+        this.updateSvgSize();
+    }
+
+    updateSvgSize() {
+        this.svgSize = window.innerWidth >= 768 ? 70 : 50;
+    }
 }
